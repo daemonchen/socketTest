d97816780b97c570180e98edd96404aab757767b
Daemon <cgyqqcgy@126.com>
2012-11-27 10:19:25 +0800

    add the ip infomation from client

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var clientName='';
-var app=require('http').createServer(function(req,res){
-	fs.readFile(__dirname+'/index.html',function(err,data){
-		if(err){
+var clientName = '';
+var app = require('http').createServer(function (req, res) {
+	fs.readFile(__dirname + '/index.html', function (err, data) {
+		if (err) {
 			res.writeHead(500);
 			return res.end('Server ERR');
 		}
@@ -10,12 +10,22 @@ var app=require('http').createServer(function(req,res){
 		//clientName=req;
 	});
 });
-var io=require('socket.io').listen(app);
-var fs=require('fs');
+var io = require('socket.io').listen(app);
+var fs = require('fs');
 app.listen(8081);
-io.sockets.on('connection',function(socket){
-	clientName=JSON.stringify(socket[1]);
-	socket.on('client',function(data){
-		socket.broadcast.emit('server',{userName:data['userName'],chat:data['chat']});
-	});	
+io.sockets.on('connection', function (socket) {
+	clientName = JSON.stringify(socket[1]);
+	var clientIp = socket.handshake.address;
+	socket.on('client', function (data) {
+		socket.emit('server', {
+			ip: clientIp.address + ':' + clientIp.port,
+			userName: data['userName'],
+			chat: data['chat']
+		});
+		socket.broadcast.emit('broadcast', {
+			ip: clientIp.address + ':' + clientIp.port,
+			userName: data['userName'],
+			chat: data['chat']
+		});
+	});
 });
